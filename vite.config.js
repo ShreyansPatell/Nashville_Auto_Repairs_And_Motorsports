4e5b95319cbc6e0e5afb9eb7fedf9882bc78e097
Shreyans Patel <shreyans.contact@gmail.com>
2025-07-13 12:10:35 -0700

    Support github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace this with your GitHub repo name
+const repoName = 'nashville-auto-repairs-and-motorsports'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,
 })
